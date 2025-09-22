1796826c175e85f405252b4e347409830ad847e8
Eric-Bastos <bastoseric09@gmail.com>
2025-09-22 10:59:38 -0300

    Delete Aula/Aula directory

--- Aula/Aula/src/main/java/com/example/Aula/Repository/RepositoryPessoa.java
+++
@@ -1,9 +0,0 @@
-package com.example.Aula.Repository;
-
-import com.example.Aula.Model.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RepositoryPessoa extends JpaRepository<Pessoa, UUID>{
-}
