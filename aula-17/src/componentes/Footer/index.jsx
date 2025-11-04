aafa451282fc1fe52a864f3fae495bfc5b3fc22b
Eric-Bastos <bastoseric09@gmail.com>
2025-11-04 10:38:01 -0300

    Add files via upload

---
+++ aula-17/src/componentes/Footer/index.jsx
@@ -0,0 +1,11 @@
+// src\componentes\Footer\index.jsx //
+
+import './style.css'
+
+export default function Footer() {
+    <footer>
+        <div>
+            <strong></strong>
+        </div>
+    </footer>
+}
---
+++ aula-17/src/componentes/Header/index.jsx
@@ -0,0 +1,1 @@
+// src\componentes\Header\index.jsx //
